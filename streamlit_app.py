68f7fcb3e61e753e984890ef03a27e2d65fa40c4
Ajeshkumarsnow <113832168+Ajeshkumarsnow@users.noreply.github.com>
2022-09-20 15:36:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import snowflake-connector
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
